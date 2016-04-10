f05d2a0063f2440801e5abacfded88dc41e77df1
Olivier Louvignes <olivier@mg-crea.com>
2016-04-10 21:53:52 +0200

    chore(test): drop chai for expect

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,9 +1,7 @@
 
 import fs from 'fs';
-import chai from 'chai';
-// import sinon from 'sinon';
 import path from 'path';
-const expect = chai.expect;
+import expect from 'expect';
 import EasyRSA from './..';
 import {pki} from 'node-forge';
 
@@ -15,8 +13,8 @@ describe('EasyRSA', () => {
   describe('#constructor()', () => {
     it('should properly merge options', () => {
       const easyrsa = new EasyRSA();
-      expect(easyrsa.config).to.be.a('object');
-      expect(easyrsa.config.pkiDir).to.eql(path.resolve(__dirname, '..', 'pki'));
+      expect(easyrsa.config).toBeA('object');
+      expect(easyrsa.config.pkiDir).toEqual(path.resolve(__dirname, '..', 'pki'));
     });
   });
   describe('#initPKI()', () => {
@@ -24,7 +22,7 @@ describe('EasyRSA', () => {
       const easyrsa = new EasyRSA(options);
       return easyrsa.initPKI({force: true}).then(() => {
         return fs.statAsync(easyrsa.config.pkiDir).call('isDirectory').then((isDirectory) => {
-          expect(isDirectory).to.be.true;
+          expect(isDirectory).toBeTrue;
         });
       });
     });
@@ -34,11 +32,11 @@ describe('EasyRSA', () => {
       const easyrsa = new EasyRSA(options);
       easyrsa.buildCA().then(({privateKey, cert}) => {
         const privateKeyPem = pki.privateKeyToPem(privateKey);
-        expect(privateKeyPem).to.be.a('string');
-        expect(privateKeyPem).to.match(/^-----BEGIN RSA PRIVATE KEY-----\r\n.+/);
+        expect(privateKeyPem).toBeA('string');
+        expect(privateKeyPem).toMatch(/^-----BEGIN RSA PRIVATE KEY-----\r\n.+/);
         const certPem = pki.certificateToPem(cert);
-        expect(certPem).to.be.a('string');
-        expect(certPem).to.match(/^-----BEGIN CERTIFICATE-----\r\n.+/);
+        expect(certPem).toBeA('string');
+        expect(certPem).toMatch(/^-----BEGIN CERTIFICATE-----\r\n.+/);
       });
     });
   });
@@ -47,11 +45,11 @@ describe('EasyRSA', () => {
       const easyrsa = new EasyRSA(options);
       easyrsa.genReq('EntityName').then(({privateKey, csr}) => {
         const privateKeyPem = pki.privateKeyToPem(privateKey);
-        expect(privateKeyPem).to.be.a('string');
-        expect(privateKeyPem).to.match(/^-----BEGIN RSA PRIVATE KEY-----\r\n.+/);
+        expect(privateKeyPem).toBeA('string');
+        expect(privateKeyPem).toMatch(/^-----BEGIN RSA PRIVATE KEY-----\r\n.+/);
         const csrPem = pki.certificationRequestToPem(csr);
-        expect(csrPem).to.be.a('string');
-        expect(csrPem).to.match(/^-----BEGIN CERTIFICATE REQUEST-----\r\n.+/);
+        expect(csrPem).toBeA('string');
+        expect(csrPem).toMatch(/^-----BEGIN CERTIFICATE REQUEST-----\r\n.+/);
       });
     });
   });
@@ -60,10 +58,10 @@ describe('EasyRSA', () => {
       const easyrsa = new EasyRSA(options);
       easyrsa.signReq('client', 'EntityName').then(({cert, serial}) => {
         const certPem = pki.certificateToPem(cert);
-        expect(certPem).to.be.a('string');
-        expect(certPem).to.match(/^-----BEGIN CERTIFICATE-----\r\n.+/);
-        expect(serial).to.be.a('string');
-        expect(serial).to.match(/[\da-f]/i);
+        expect(certPem).toBeA('string');
+        expect(certPem).toMatch(/^-----BEGIN CERTIFICATE-----\r\n.+/);
+        expect(serial).toBeA('string');
+        expect(serial).toMatch(/[\da-f]/i);
       });
     });
   });
